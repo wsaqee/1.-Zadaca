8b37cdcc44549a1eb92083e20f5c00416653a62c
Matija Nestic <matija.nestic@gmail.com>
2015-12-13 22:29:11 +0100

    Prva DZ-commit

---
+++ ZAD1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntegerList Lista1 = new IntegerList();
+            Lista1.Add(1);
+            Lista1.Add(2);
+            Lista1.Add(3);
+            Lista1.Add(4);
+            Lista1.Add(5);
+
+            IspisiElemente(Lista1);
+
+            Lista1.RemoveAt(0);
+            IspisiElemente(Lista1);
+
+            Lista1.Remove(5);
+            IspisiElemente(Lista1);
+
+            Console.WriteLine("Number of elements " + Lista1.Count + "\n");
+
+            Console.WriteLine("Lista1.Remove(100)");
+            Console.WriteLine(Lista1.Remove(100) + "\n");
+
+            Console.WriteLine("Lista1.RemoveAt(5)");
+            Console.WriteLine(Lista1.RemoveAt(5) + "\n");
+
+            Lista1.Clear();
+
+            IspisiElemente(Lista1);
+
+        }
+
+        static void IspisiElemente (IntegerList L1)
+        {
+            for (int i = 0; i < L1.Count; i++)
+            {
+                Console.WriteLine(L1.GetElement(i));
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ ZAD5/CollisionDetector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace ZAD5
+{
+    public class CollisionDetector
+    {
+        /// <summary>
+        /// Calculates if rectangles describing two sprites
+        /// are overlapping on screen.
+        /// </summary>
+        /// <param name="s1">First sprite</param>
+        /// <param name="s2">Second sprite</param>
+        /// <returns>Returns true if overlapping</returns>
+        public static bool Overlaps(Sprite s1, Sprite s2)
+        {
+            Rectangle RectA = new Rectangle((int)s1.Position.X, (int)s1.Position.Y, s1.Size.Width, s1.Size.Height);
+            Rectangle RectB = new Rectangle((int)s2.Position.X, (int)s2.Position.Y, s2.Size.Width, s2.Size.Height);
+            if (Rectangle.Intersect(RectA, RectB) != Rectangle.Empty) return true;
+            return false;
+        }
+    }
+}
